56c7d77f154e4fc39d5d9c1871ceeb96cb81853a
Viacheslav Chernogorov <slavachernogorv@mail.ru>
2022-12-08 19:10:54 +0300

    fix: get JWT secret back

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -135,7 +135,7 @@ module.exports.login = (req, res, next) => {
 
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'key-secret', { expiresIn: '7d' });
+      const token = jwt.sign({ _id: user._id }, process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'key-secret', { expiresIn: '7d' });
       res.send({ token });
     })
     .catch(next);
